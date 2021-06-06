b836bf86e6c9d04b0cd8418960b21eeb59a8c9f9
yli-qlc <yli@quantumlogik.com>
2021-06-05 23:59:07 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import holoviews as hv
+hv.extension('bokeh')
+from bokeh.plotting import show
+
+
+
+df = pd.read_csv(r"C:\Users\YingLi\PycharmProjects\vaccines\vaccines_data.csv")
+value_list = ["EU","COVAX","USA","UK","Brazil","China","India","Canada","Japan","Latin America w/o Brazil","African Union"]
+boolean_series = df.Country.isin(value_list)
+df = df[boolean_series]
+df['Doses'] = df['Doses'].div(1000000000).round(2)
+
+
+
+sankey1 = hv.Sankey(df,kdims=["Manufacturer", "Country"], vdims=["Doses"])
+show (hv.render(sankey1))
+
+sankey2 = hv.Sankey(df,kdims=["Manufacturer", "Country"], vdims=["Doses"])
+sankey2.opts(cmap="PuBuGn_r",label_position='outer',
+                                 edge_color='Manufacturer', edge_line_width=0,
+                                 node_alpha=1.0, node_width=40, node_sort=False,
+                                 width=800, height=1200, bgcolor="snow",
+                                 title="How many covid-19 vaccines has each manufacturer sold")
+
+show (hv.render(sankey2))
